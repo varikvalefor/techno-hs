83eb0bef0bfe6d71c97e6470233a99721441df0c
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-12 16:34:51 -0500

    techno.hs: a (b c) -> a $ b c.

--- techno.hs
+++ techno.hs
@@ -6,6 +6,6 @@ giveMeALine x = putStr (x:"") >> drawCursor;
 drawCursor :: IO ();
 drawCursor = mapM_ g "\" \BS_ \BS"
   where g x | x == ' ' = threadDelay 33333
-            | otherwise = putStr (x:"")
+            | otherwise = putStr x:""
 
 main = mapM_ giveMeALine "TECHNO   " >> main;
