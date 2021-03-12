ffc7dea8fb78df6c851a4b151f6ca74a3a8bccd8
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-12 16:29:45 -0500

    techno.hs: theString -> string literal.

--- techno.hs
+++ techno.hs
@@ -3,12 +3,9 @@ import Control.Concurrent;
 giveMeALine :: Char -> IO ();
 giveMeALine x = putStr (x:"") >> drawCursor;
 
-theString :: String;
-theString = "TECHNO   ";
-
 drawCursor :: IO ();
 drawCursor = mapM_ g "\" \BS_ \BS"
   where g x | x == ' ' = threadDelay 50000
             | otherwise = putStr (x:"")
 
-main = mapM_ (giveMeALine) theString >> main;
+main = mapM_ (giveMeALine) "TECHNO   " >> main;
