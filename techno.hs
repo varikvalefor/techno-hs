0c16b0d3fed1a4ba50487e804a4a4c2b27870fc2
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-12 16:25:38 -0500

    Create techno.hs

---
+++ techno.hs
@@ -0,0 +1,17 @@
+import Control.Concurrent;
+
+giveMeALine :: Char -> IO ();
+giveMeALine x = putStr (x:"") >> threadDelay 12500 >> drawCursor;
+
+theString :: String;
+theString = "TECHNO   ";
+
+printThing :: Char -> IO ();
+printThing a = giveMeALine a >> drawCursor;
+
+drawCursor :: IO ();
+drawCursor = mapM_ g "' \BS, \BS"
+  where g x | x == ' ' = threadDelay 50000
+            | otherwise = putStr (x:"")
+
+main = mapM_ (giveMeALine) theString >> main;
