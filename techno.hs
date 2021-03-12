5893e7ee8a33dadbecb02ee05be3a0c49e6a4594
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-12 16:31:04 -0500

    techno.hs: The threadDelay is slightly reduced.

--- techno.hs
+++ techno.hs
@@ -5,7 +5,7 @@ giveMeALine x = putStr (x:"") >> drawCursor;
 
 drawCursor :: IO ();
 drawCursor = mapM_ g "\" \BS_ \BS"
-  where g x | x == ' ' = threadDelay 50000
+  where g x | x == ' ' = threadDelay 33333
             | otherwise = putStr (x:"")
 
 main = mapM_ (giveMeALine) "TECHNO   " >> main;
